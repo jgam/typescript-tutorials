4611c8b875bb9d49b4f4978e77f61d9ed299f81a
jgam <jgam@alumni.nd.edu>
2020-06-19 00:20:07 +0900

    ts tutorial1 done

--- src/pratice.js
+++ src/pratice.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var count = 0;
 count += 1;
 // count = '문자열';
@@ -9,3 +20,35 @@ var messages = ['hello', 'world'];
 var mightBeUndefined = undefined;
 mightBeUndefined = 'hey';
 var nullabaleNumber = null;
+//write sum function taking two params x and y
+function TwoSum(x, y) {
+    return x + y;
+}
+//console.log(TwoSum(1, '1'));
+console.log(TwoSum(1, 3));
+//write sumArray taking numbers
+function sumArray(numbers) {
+    return numbers.reduce(function (acc, current) { return acc + current; }, 0);
+}
+var total = sumArray([1, 2, 3, 4, 5]);
+console.log(total);
+function merge(a, b) {
+    return __assign({}, a, b);
+}
+var merged = merge({ foo: 1 }, { bar: 2 });
+merged.bar;
+// param이 정해져있지 않을 때
+function wrap(param) {
+    return {
+        param: param
+    };
+}
+var wrapped = wrap('aaa');
+wrapped.param;
+var itemss = {
+    list: ['a', 'b']
+};
+var items = {
+    list: ['a', 'b'],
+    value: 'aaa'
+};
--- src/pratice.ts
+++ src/pratice.ts
@@ -16,9 +16,56 @@ mightBeUndefined = 'hey';
 let nullabaleNumber: number | null = null;
 
 //write sum function taking two params x and y
+function TwoSum(x: number, y: number): number {
+  return x + y;
+}
+//console.log(TwoSum(1, '1'));
+console.log(TwoSum(1, 3));
 
 //write sumArray taking numbers
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
 
-//write return nothing
+const total = sumArray([1, 2, 3, 4, 5]);
 
-//write class circle with radius and getArea as method
+console.log(total);
+
+function merge<A, B>(a: A, b: B) {
+  return {
+    ...a,
+    ...b,
+  };
+}
+const merged = merge({ foo: 1 }, { bar: 2 });
+merged.bar;
+
+// param이 정해져있지 않을 때
+function wrap<T>(param: T) {
+  return {
+    param,
+  };
+}
+
+const wrapped = wrap('aaa');
+
+wrapped.param;
+
+interface Itemss<T> {
+  list: T[];
+}
+
+const itemss: Itemss<string> = {
+  list: ['a', 'b'],
+};
+
+// 타입을 지켜내고싶을 때 제네릭을 사용한다.
+type Items<T, V> = {
+  list: T[];
+  value: V;
+};
+
+const items: Items<string, string> = {
+  list: ['a', 'b'],
+  value: 'aaa',
+};
